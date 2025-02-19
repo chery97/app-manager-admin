47717856e09db3e132dfdb1270cbab54551eed8b
aalon <aaron@geekstudio.co.kr>
2025-02-19 11:15:37 +0900

    fix: 파트너 등록 내 탭 분리

--- src/views/pages/partner/PartnerRegister.js
+++ src/views/pages/partner/PartnerRegister.js
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { CButton, CCol, CForm, CFormInput, CFormSelect, CRow } from '@coreui/react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CForm,
+  CFormInput,
+  CFormSelect, CNav,
+  CNavItem, CNavLink,
+  CRow, CTab, CTabContent, CTabList, CTabPanel, CTabs
+} from '@coreui/react'
 import { useParams } from 'react-router-dom'
 import user from 'src/api/user'
 
@@ -35,89 +46,153 @@ const PartnerRegister = () => {
   const handleSubmit = async (e) => {}
 
   return (
-    <CForm className="row g-3" onSubmit={handleSubmit}>
-      <CRow>
-        <CCol className="bg-dark p-3">
-          <CRow className="mb-3">
-            <CCol>
-              <CFormInput
-                type="email"
-                id="id"
-                label="아이디"
-                value={userData.id}
-                style={{ width: '100%' }}
-                readOnly
-              />
-            </CCol>
-            <CCol>
-              <CFormInput
-                type="text"
-                id="userName"
-                label="이름"
-                value={userData.userName}
-                style={{ width: '100%' }}
-                onChange={handleChange}
-              />
-            </CCol>
-            <CCol>
-              <CFormInput
-                id="userTel"
-                label="연락처"
-                value={userData.userTel}
-                placeholder="01012345678"
-                style={{ width: '100%' }}
-                onChange={handleChange}
-              />
-            </CCol>
-          </CRow>
-          <CRow className="mb-3">
-            <CCol>
-              <CFormSelect
-                id="managerType"
-                label="타입"
-                value={userData.managerType}
-                style={{ width: '100%' }}
-                onChange={handleChange}
-                readOnly
-              >
-                <option value="partner">파트너</option>
-                <option value="admin">관리자</option>
-              </CFormSelect>
-            </CCol>
-            <CCol>
-              <CFormInput
-                id="partnerSno"
-                label="파트너명"
-                value={userData.partnerSno}
-                placeholder="input Partner's Name"
-                style={{ width: '100%' }}
-                onChange={handleChange}
-              />
-            </CCol>
-          </CRow>
-          <CRow className="mb-3">
-            <CCol>
-              <CFormInput
-                id="userEmail"
-                label="이메일"
-                value={userData.userEmail}
-                placeholder="test@domain.com"
-                style={{ width: '100%' }}
-                onChange={handleChange}
-              />
-            </CCol>
-          </CRow>
-        </CCol>
+    <CCard className="mb-4">
+      <CCardHeader className="d-flex align-items-center">
+        <CTabs activeItemKey="profile">
+          <CTabList variant="tabs">
+            <CTab itemKey="home">
+              <strong>파트너 등록</strong>
+            </CTab>
+            <CTab itemKey="profile">
+              <strong>앱 관리</strong>
+            </CTab>
+          </CTabList>
+          <CTabContent>
+            <CTabPanel className="p-3" itemKey="home">
+              <CCardBody>
+                <CForm className="row g-3" onSubmit={handleSubmit}>
+                  <CCol className="bg-dark p-3">
+                    <CRow>
+                      <CCol>
+                        <CFormInput
+                          type="email"
+                          id="id"
+                          label="아이디"
+                          value={userData.id}
+                          style={{ width: '100%' }}
+                          readOnly
+                        />
+                      </CCol>
+                      <CCol>
+                        <CFormInput
+                          type="text"
+                          id="userName"
+                          label="이름"
+                          value={userData.userName}
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                        />
+                      </CCol>
+                      <CCol>
+                        <CFormInput
+                          id="userTel"
+                          label="연락처"
+                          value={userData.userTel}
+                          placeholder="01012345678"
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                        />
+                      </CCol>
+                    </CRow>
+                    <CRow className="mb-3">
+                      <CCol>
+                        <CFormSelect
+                          id="managerType"
+                          label="타입"
+                          value={userData.managerType}
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                          readOnly
+                        >
+                          <option value="partner">파트너</option>
+                          <option value="admin">관리자</option>
+                        </CFormSelect>
+                      </CCol>
+                      <CCol>
+                        <CFormInput
+                          id="partnerSno"
+                          label="파트너명"
+                          value={userData.partnerSno}
+                          placeholder="input Partner's Name"
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                        />
+                      </CCol>
+                    </CRow>
+                    <CRow className="mb-3">
+                      <CCol>
+                        <CFormInput
+                          id="userEmail"
+                          label="이메일"
+                          value={userData.userEmail}
+                          placeholder="test@domain.com"
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                        />
+                      </CCol>
+                    </CRow>
+                  </CCol>
 
-        <CCol className="bg-dark p-3"></CCol>
+                  <CCol className="bg-dark p-3"></CCol>
 
-        <CCol xs={12} className="mt-3">
-          <CButton color="primary" type="submit">
-            저장
-          </CButton>
-        </CCol>
-      </CRow>
-    </CForm>
+                  <CCol xs={12} className="mt-3 d-flex align-items-center">
+                    <CButton color="primary" type="submit" className="ms-auto">
+                      저장
+                    </CButton>
+                  </CCol>
+                </CForm>
+              </CCardBody>
+            </CTabPanel>
+            <CTabPanel className="p-3" itemKey="profile">
+              <CCardBody>
+                <CForm className="row g-3" onSubmit={handleSubmit}>
+                  <CCol className="bg-dark p-3">
+                    <CRow>
+                      <CCol>
+                        <CFormInput
+                          type="email"
+                          id="id"
+                          label="아이디"
+                          value={userData.id}
+                          style={{ width: '100%' }}
+                          readOnly
+                        />
+                      </CCol>
+                      <CCol>
+                        <CFormInput
+                          type="text"
+                          id="userName"
+                          label="이름"
+                          value={userData.userName}
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                        />
+                      </CCol>
+                      <CCol>
+                        <CFormInput
+                          id="userTel"
+                          label="연락처"
+                          value={userData.userTel}
+                          placeholder="01012345678"
+                          style={{ width: '100%' }}
+                          onChange={handleChange}
+                        />
+                      </CCol>
+                    </CRow>
+                  </CCol>
+                  <CCol className="bg-dark p-3"></CCol>
+                  <CCol xs={12} className="mt-3 d-flex align-items-center">
+                    <CButton color="primary" type="submit" className="ms-auto">
+                      저장
+                    </CButton>
+                  </CCol>
+                </CForm>
+              </CCardBody>
+            </CTabPanel>
+          </CTabContent>
+        </CTabs>
+      </CCardHeader>
+    </CCard>
   )
 }
 export default PartnerRegister

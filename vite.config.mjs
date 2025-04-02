cccad2473068479e3a4bb3db1d175275031e38f5
Jay <jay@geekstudio.co.kr>
2025-04-02 12:09:20 +0900

    :wrench: Fix: 기본 포트 8162로 변경

--- vite.config.mjs
+++ vite.config.mjs
@@ -49,7 +49,7 @@ export default defineConfig(() => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
     server: {
-      port: 3000,
+      port: 8162,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
